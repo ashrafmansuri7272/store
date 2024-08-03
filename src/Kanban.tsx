1bce4de7566030f3c584fe83ed02f0722ad5383c
ashrafmansuri7272 <ashrafmansuri91@gmail.com>
2024-08-03 11:25:09 +0530

    deleteIndex imlemented

--- src/Kanban.tsx
+++ src/Kanban.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import Column from "./Column";
- 
+
 
 const Kanban: React.FC = () => {
     const columns = ['backlog', 'todo', 'ongoing', 'done'];
 
     const [tasks, setTasks] = useState({
-        backlog: [{id: 0, text: 'task0'}],
+        backlog: [{ id: 0, text: 'task0' }],
         todo: [],
         ongoing: [],
         done: []
     })
-    const [newTask, setNewTask] =useState('')
+    const [newTask, setNewTask] = useState('')
 
     const moveTask = (taskId, direction) => {
         let taskFound;
@@ -19,16 +19,17 @@ const Kanban: React.FC = () => {
             taskFound = tasks[col].find(tas => tas.id === taskId)
             return !!taskFound
         })
-        const targetColumn = columns[columns.indexOf(fromColumn)+1];
-        setTasks(prevTasks=>({
+        const targetColumn = columns[columns.indexOf(fromColumn) + direction];
+        if (!targetColumn) return
+        setTasks(prevTasks => ({
             ...prevTasks,
-            [fromColumn]: prevTasks[fromColumn].filter(tass=> tass.id !== taskId),
+            [fromColumn]: prevTasks[fromColumn].filter(tass => tass.id !== taskId),
             [targetColumn]: [...prevTasks[targetColumn], taskFound]
         }))
-       
-    }   
+    }
+
     const addTask = () => {
-        if(newTask.trim() === '') return
+        if (newTask.trim() === '') return
         const newTaskObj = {
             id: Date.now(),
             text: newTask
@@ -39,20 +40,31 @@ const Kanban: React.FC = () => {
         }))
     }
     const deleteTask = (taskId) => {
-        
+        let deletetaskIndex;
+        setTasks(prevTasks => {
+            const currentColumn = columns.find(col => {
+                const deletetaskIndex = tasks[col].findIndex(t => t.id === taskId)
+                return !!deletetaskIndex
+            })
+            if (!deletetaskIndex) return prevTasks;
+            return {
+                ...prevTasks,
+                [currentColumn]: prevTasks[currentColumn].splice(deletetaskIndex, 1)
+            }
+        })
     }
 
-return(
-    <>
-    <input type='text' placeholder='add New task' value={newTask} onChange={(e)=>setNewTask(e.target.value)}></input>
-    <button onClick={addTask}>Create task</button>
-   <div className="Kanban-container">
-    <Column name='Backlog' tasks={tasks.backlog} moveTask={moveTask} deleteTask={deleteTask}/>
-    <Column name='To Do' tasks={tasks.todo}  moveTask={moveTask} deleteTask={deleteTask}/>
-    <Column name='Ongoing' tasks={tasks.ongoing}  moveTask={moveTask} deleteTask={deleteTask}/>
-    <Column name='Done' tasks={tasks.done}  moveTask={moveTask} deleteTask={deleteTask}/>
-   </div>
-   </>)
+    return (
+        <>
+            <input type='text' placeholder='add New task' value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
+            <button onClick={addTask}>Create task</button>
+            <div className="Kanban-container">
+                <Column name='Backlog' tasks={tasks.backlog} moveTask={moveTask} deleteTask={deleteTask} />
+                <Column name='To Do' tasks={tasks.todo} moveTask={moveTask} deleteTask={deleteTask} />
+                <Column name='Ongoing' tasks={tasks.ongoing} moveTask={moveTask} deleteTask={deleteTask} />
+                <Column name='Done' tasks={tasks.done} moveTask={moveTask} deleteTask={deleteTask} />
+            </div>
+        </>)
 }
 
 export default Kanban
